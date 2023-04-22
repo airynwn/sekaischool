de2b808529826259f00e78a885998e9cd6abfad0
airynnn <laura.alcon@iesdonana.org>
2023-04-23 00:01:43 +0200

    R37: Vista de admin WIP

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,17 +1,11 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Portal de administración') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Hola") }}
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+@extends('layouts.admin')
+@section('title')
+    Portal de administración ♪ SekaiSchool
+@endsection
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
+@endsection
+@extends('layouts.navbar')
+@section('content')
+    <h1>holaaa<h1>
+@endsection
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,66 @@
+<nav class="navbar navbar-expand-lg navbar-dark header">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <picture>
+                <img id="logo" class="brillo" src="img/logotipo.svg" alt="Logo SekaiSchool">
+            </picture>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0 mx-auto">
+                @if (auth()->user()->admin)
+                    <li class="nav-item">
+                        <a class="nav-link active brillo" href="{{ route('admin.index' )}}">Admin</a>
+                    </li>
+                @endif
+                <li class="nav-item">
+                    <a class="nav-link active brillo" href="{{ route('dashboard') }}">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active brillo" href="cartas.html">Cartas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active brillo" href="#modal-personajes">Personajes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active brillo" href="comunidad.html">Comunidad</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link active dropdown-toggle brillo" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Grupos
+                    </a>
+                    <ul class="dropdown-menu header noborder">
+                    <li><a class="dropdown-item text-white brillo" href="ln.html">Leo/need</a></li>
+                    <li><a class="dropdown-item text-white brillo" href="mmj.html">MORE MORE JUMP!</a></li>
+                    <li><a class="dropdown-item text-white brillo" href="vbs.html">Vivid BAD SQUAD</a></li>
+                    <li><a class="dropdown-item text-white brillo" href="wxs.html">Wonderlands x Showtime</a></li>
+                    <li><a class="dropdown-item text-white brillo" href="25ji.html">25-ji, Nightcord de.</a></li>
+                    </ul>
+                </li>
+            </ul>
+            @if (auth())
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link active dropdown-toggle brillo" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        {{ auth()->user()->name }}
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end">
+                        <li><a class="dropdown-item" href="{{ route('profile.edit') }}">Perfil</a></li>
+                        <li><a class="dropdown-item" href="{{ route('dashboard') }}">Dashboard</a></li>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();this.closest('form').submit();">Cerrar sesión</a></li>
+                        </form>
+                    </ul>
+                </li>
+            </ul>
+            @else
+            <span class="navbar-text">
+                <a href="{{ route('login') }}" class="login brillo">Iniciar sesión</a>
+            </span>
+            @endif
+        </div>
+    </div>
+</nav>
