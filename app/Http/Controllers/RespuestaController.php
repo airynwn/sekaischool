996747aba8574bcea14d63c5ad1d67dfb246e842
airynnn <laura.alcon@iesdonana.org>
2023-04-29 13:32:02 +0200

    R37: Crea controllers

---
+++ app/Http/Controllers/RespuestaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Respuesta;
+use Illuminate\Http\Request;
+
+class RespuestaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Respuesta  $respuesta
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Respuesta $respuesta)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Respuesta  $respuesta
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Respuesta $respuesta)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Respuesta  $respuesta
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Respuesta $respuesta)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Respuesta  $respuesta
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Respuesta $respuesta)
+    {
+        //
+    }
+}
