300319bba2e6aa6bc364d6785f7a0219892d1027
airynnn <laura.alcon@iesdonana.org>
2023-05-16 22:53:57 +0200

    R55: Arregla comportamiento de borrado de valoraciones

--- database/migrations/2023_04_16_190505_create_valoraciones_table.php
+++ database/migrations/2023_04_16_190505_create_valoraciones_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->integer('post_id');
             $table->integer('user_id');
             // [FK] Post + User
-            $table->foreign('post_id')->references('id')->on('posts');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users');
         });
     }
