5a98ce1123de418d2a7880e7969711e426f43306
airynnn <laura.alcon@iesdonana.org>
2023-05-21 03:41:11 +0200

    R52: Personalización del perfil de usuario (WIP estilo+validación)

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -4,7 +4,7 @@
 @endsection
 @section('css')
     <link rel="stylesheet" href="{{ asset('css/perfil.css') }}">
-    <script type="text/javascript" src="{{ asset('js/editarPerfil.js') }}" defer></script>
+    <script type="text/javascript" src="{{ asset('js/sekaischool.js') }}" defer></script>
 @endsection
 {{-- Main --}}
 @section('content')
@@ -31,6 +31,81 @@
                 value="{{ auth()->user()->twitter }}">
             </div>
         </div>
+        {{-- <div class="row espacio">
+            <div class="col">
+                <a class="opcion opcion-redondeado" href="#modal-personajes">Personaje</a>
+                <img src="{{ auth()->user()->pj_fav->chibi ?? '' }}"
+                alt="{{ auth()->user()->name . ' Personaje Fav' }}">
+            </div>
+            {{-- aquilo s grpos --}}
+            {{-- TODO details? --}}
+            <div class="col">
+                <a class="opcion opcion-redondeado" href="#modal-personajes">Grupo</a>
+                <img src="{{ auth()->user()->grupo_fav->imagen ?? '' }}"
+                alt="{{ auth()->user()->name . ' Grupo Fav' }}">
+            </div>
+        </div> --}}
+        {{-- TESTESTEST --}}
+        <div class="row espacio">
+            <div class="col">
+                <details>
+                    <summary>Personajes</summary>
+                    <div class="details-flex">
+                    </div>
+                    <div class="container caja-content">
+                        <div class="row row-cols-3 row-cols-md-5 row-cols-lg-6 g-3">
+                            @foreach ($personajes as $pj)
+                                <div class="col">
+                                    <label>
+                                        <input type="radio" name="pj_fav_id" value="{{ $pj->id }}"
+                                        {{ $pj->id == auth()->user()->pj_fav_id ? 'checked' : '' }}
+                                        {{-- TODO estilar radio --}}
+                                        {{-- style="appearance: none;" --}}
+                                        >
+                                        <img src="{{ asset($pj->chibi) }}" alt="Chibi {{ $pj->nombre }}"
+                                        class="img-fluid"
+                                        {{-- TODO funcion que solo permita 1 selected --}}
+                                        {{-- onclick="seleccionarUnico(this)" --}}
+                                        >
+                                    </label>
+                                </div>
+                          @endforeach
+                        </div>
+                    </div>
+                </details>
+            </div>
+        </div>
+        {{-- TESTESTEST --}}
+        <div class="row espacio">
+            <div class="col">
+                <details>
+                    <summary>Grupo</summary>
+                    <div class="details-flex">
+                    </div>
+                    <div class="container caja-content">
+                        <div class="row row-cols-3 row-cols-md-5 row-cols-lg-6 g-3">
+                            @foreach ($grupos as $grupo)
+                                <div class="col">
+                                    <label>
+                                        <input type="radio" name="grupo_fav_id" value="{{ $grupo->id }}"
+                                        {{ $grupo->id == auth()->user()->grupo_fav_id ? 'checked' : '' }}
+                                        {{-- TODO estilar radio --}}
+                                        {{-- style="appearance: none;" --}}
+                                        >
+                                        <img src="{{ asset($grupo->imagen) }}" alt="Imagen {{ $grupo->nombre }}"
+                                        class="img-fluid"
+                                        {{-- TODO funcion que solo permita 1 selected --}}
+                                        {{-- onclick="seleccionarUnico(this)" --}}
+                                        >
+                                    </label>
+                                </div>
+                          @endforeach
+                        </div>
+                    </div>
+                </details>
+            </div>
+        </div>
+        {{--  --}}
         <div class="row espacio">
             <div class="col">
                 <button type="submit">Enviar</button>
@@ -45,4 +120,5 @@
 <div>
     @include('profile.partials.update-password-form')
 </div> --}}
+@include('layouts.modal', ['personajes' => $personajes])
 @endsection
