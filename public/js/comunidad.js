68eec8ab7442085dc73f165b86061560ba8a0441
airynnn <laura.alcon@iesdonana.org>
2023-05-28 15:39:57 +0200

    R57: Filtra publicaciones (orden de reciente/popularidad) por AJAX

--- public/js/comunidad.js
+++ public/js/comunidad.js
@@ -159,3 +159,37 @@ async function eliminarPost(event) {
 
     form.reset();
 }
+
+async function mostrarPosts(event) {
+    // Objeto con los datos del formulario (_token, modo)
+    let formData = new FormData();
+
+    let modo = event.target.getAttribute("data-modo");
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+    formData.append('_token', csrfToken);
+    formData.append('modo', modo);
+
+    console.log(modo);
+
+    await fetch('/comunidad/posts', {
+        method: "POST",
+        body: formData,
+      })
+      .then(response => {
+        if (response.ok) {
+            return response.text();
+        } else {
+            throw new Error(response.text());
+        }
+    })
+      .then(result => {
+        console.log(result)
+        let contenedor = document.getElementById("post-container");
+        contenedor.innerHTML = result;
+    })
+      .catch(e => {
+        console.log(e);
+        return "Se ha producido un error. Inténtelo más tarde.";
+    });
+}
