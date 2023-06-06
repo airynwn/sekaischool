b171aabfa066eac9f71637a15d7cdda99910389c
airynnn <laura.alcon@iesdonana.org>
2023-06-06 19:27:36 +0200

    R65: Calendario listo - Muestra eventos en los cumpleaños

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.guest')
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
     <link rel="stylesheet" href="{{ asset('css/colores.css') }}">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'>
@@ -10,32 +10,39 @@
       document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
         var calendar = new FullCalendar.Calendar(calendarEl, {
-          initialView: 'dayGridMonth',
-          themeSystem: 'bootstrap5',
-          locale: 'es',
-          timeZone: 'local',
-          events: function(fetchInfo, successCallback, failureCallback) {
-            fetch('/cumples')
-                .then(response => response.json())
-                .then(data => {
-                    successCallback(data);
-                    console.log(data)
-                })
-                .catch(error => {
-                    failureCallback(error);
-                });
-        },
-        buttonText: {
-            today:    'hoy',
-            month:    'mes',
-            week:     'semana',
-            day:      'día',
-            list:     'lista'
-        },
-        validRange: {
-            start: '2023-01-01',
-            end: '2023-12-31'
-        }
+            initialView: 'dayGridMonth',
+            themeSystem: 'bootstrap5',
+            locale: 'es',
+            timeZone: 'local',
+            aspectRatio: 1.75,
+            events: function(fetchInfo, successCallback, failureCallback) {
+                fetch('/cumples')
+                    .then(response => response.json())
+                    .then(data => {
+                        successCallback(data);
+                        console.log(data)
+                    })
+                    .catch(error => {
+                        failureCallback(error);
+                    });
+            },
+            eventDidMount: function(info) {
+                info.el.setAttribute("data-bs-toggle", "tooltip");
+                info.el.setAttribute("title", "");
+                info.el.setAttribute("data-bs-original-title", `Cumpleaños de ${info.event.title}`);
+                const tooltip = new bootstrap.Tooltip(info.el);
+            },
+            buttonText: {
+                today:    'hoy',
+                month:    'mes',
+                week:     'semana',
+                day:      'día',
+                list:     'lista'
+            },
+            validRange: {
+                start: '2023-01-01',
+                end: '2023-12-31'
+            }
         });
         calendar.render();
       });
@@ -43,15 +50,5 @@
     </script>
 @endsection
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <div x-data="{ events: [] }"
-                x-init="events = await (await fetch('/cumples')).json()">
-                <div id="calendar"></div>
-            </div>
-        </div>
-    </div>
-</div>
-
+<div id="calendar"></div>
 @endsection
